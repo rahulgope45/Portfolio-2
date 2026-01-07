d5cab406f8a7c6eb55e3d964b147114ec35d8841
rahulgope45 <rahuleile456@gmail.com>
2026-01-07 23:08:20 +0530

    Project init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Trianglyfy from '../componets/Trianglyfy'
+
+function Home() {
+  return (
+    <div>
+        
+       <div className='h-[200px] w-[200px]'>
+         <Trianglyfy/> 
+       </div>
+    </div>
+  )
+}
+
+export default Home
