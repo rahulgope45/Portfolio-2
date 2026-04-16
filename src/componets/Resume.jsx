bc57ec5a1149386415e36447c2227cb58f78f444
rahulgope45 <rahuleile456@gmail.com>
2026-04-16 13:34:10 +0530

    Resume update

--- src/componets/Resume.jsx
+++ src/componets/Resume.jsx
@@ -22,7 +22,7 @@ function Resume() {
           {/* Button */}
           <AnimatedContent>
             <a
-              href="/RahuldevA16.pdf"
+              href="/RahuldevA17.pdf"
               download
               className="inline-flex items-center justify-center
                    px-6 py-2 rounded-full
@@ -55,7 +55,7 @@ function Resume() {
           {/* Button */}
           <AnimatedContent>
             <a
-              href="/Rahuldevjan15.pdf"
+              href="/RahuldevA17.pdf"
               download
               className="inline-flex items-center justify-center
                    px-6 py-2 rounded-full
